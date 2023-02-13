fb2f20bca6d025fb664f62836054cd374d3cbd2b
ItzCandra23 <95993220+ItzCandra23@users.noreply.github.com>
2023-02-13 19:36:05 +0700

    Update 1.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,143 @@
-import { NetworkIdentifier } from "bdsx/bds/networkidentifier";
 import { ServerPlayer } from "bdsx/bds/player";
-import { command } from "bdsx/command";
-import { events } from "bdsx/event";
-import { ScoreboardX } from "./scorebase";
-require("./scorebase");
-require("./addon");
+import { ScoreAddon } from "./addon";
+import { send } from "./utils/message";
+import * as path from "path";
+import * as fs from "fs";
 
-const players = new Map<NetworkIdentifier, ServerPlayer>();
+export type ScoreLine = [string, number];
 
-events.playerJoin.on((ev) => {
-    players.set(ev.player.getNetworkIdentifier(), ev.player);
-});
+export interface ScoreboardData {
+    titles: string[];
+    lines: ScoreLine[];
+}
 
-events.playerLeft.on((ev) => {
-    players.delete(ev.player.getNetworkIdentifier());
-});
+const configPath = path.join(__dirname, "..", "config.json");
+let config: {
+    scoreboard: ScoreboardData;
+} = {
+    scoreboard: {
+        titles: [
+            "&k&lii&r&a&lScoreboardX&f&k&lii&r",
+            "&k&lii&r&d&lScoreboardX&f&k&lii&r",
+            "&k&lii&r&6&lScoreboardX&f&k&lii&r",
+            "&k&lii&r&b&lScoreboardX&f&k&lii&r",
+        ],
+        lines: [
+            ["                           ", 0],
+            ["  Name: &a{name}", 1],
+            ["  Item: &a{item_custom_name}", 2],
+            ["  Count: &a{item_count}", 3],
+            ["  Ping: &a{ping}", 4],
+            ["  Online: &a{online}/{max_online}", 5],
+            ["  Pos: &a{x}, {y}, {z}", 6],
+            ["                          ", 7],
+        ],
+    },
+};
+let scoreboard = config.scoreboard;
 
-const intrval = setInterval(() => {
-    for (const [netId, player] of players.entries()) {
-        ScoreboardX.sendTo(player);
+try { config = require(configPath) } catch(err) {}
+
+export namespace ScoreboardX {
+    /**Add a new title. */
+    export function addTitle(title: string): void {
+        scoreboard.titles.push(title);
     }
-}, 1000);
 
-events.serverClose.on(() => {
-    clearInterval(intrval);
-    ScoreboardX.save();
-});
+    /**Set title. */
+    export function setTitle(title: string, index: number): boolean {
+        if (index > scoreboard.titles.length-1||index < 0) return false;
 
-command.register("scorex", "Show or hide your scoreboard.")
-.overload((p, o) => {
-    const player = o.getEntity() as ServerPlayer;
+        scoreboard.titles[index] = title;
+        return true;
+    }
 
-    if (p.mode === "hide") ScoreboardX.setHide(player, true);
-    if (p.mode === "show") ScoreboardX.setHide(player, false);
-}, {
-    mode: command.enum("ScoreboardX", "hide", "show"),
-});
+    /**Get all scoreboard titles. */
+    export function getTitles(): string[] {
+        return scoreboard.titles;
+    }
 
-command.register("sbx", "Show or hide your scoreboard.")
-.overload((p, o) => {
-    const player = o.getEntity() as ServerPlayer;
+    /**Get random title. */
+    export function getRandomTitle(player: ServerPlayer): string {
+        const title = scoreboard.titles[Math.floor(Math.random() * scoreboard.titles.length)];
+        let addons = ScoreAddon.getAddons(player);
+        let newTitle: string = title;
 
-    if (p.mode === "hide") ScoreboardX.setHide(player, true);
-    if (p.mode === "show") ScoreboardX.setHide(player, false);
-}, {
-    mode: command.enum("ScoreboardX", "hide", "show"),
-});
+        for (const [i, addon] of addons.entries()) {
+            newTitle=newTitle.replace(new RegExp(addon[0], "g"), addon[1].toString());
+        }
+
+        return newTitle;
+    }
+
+    /**Add a new line. */
+    export function addLine(line: ScoreLine): void {
+        scoreboard.lines.push(line);
+    }
+
+    /**Set line. */
+    export function setLine(line: ScoreLine, index: number): boolean {
+        if (index > scoreboard.lines.length-1||index < 0) return false;
+
+        scoreboard.lines[index] = line;
+        return true;
+    }
+
+    /**Set all lines. */
+    export function setLines(lines: ScoreLine[]): void {
+        scoreboard.lines=lines;
+    }
+
+    /**Get all scoreboard lines. */
+    export function getLines(player: ServerPlayer): ScoreLine[] {
+        const data = new Map<number, ScoreLine>();
+        let addons = ScoreAddon.getAddons(player);
+
+        for (let [i, line] of scoreboard.lines.entries()) {
+            for (const [i_, addon] of addons.entries()) {
+                const newLine: ScoreLine = [line[0].replace(new RegExp(addon[0], "g"), addon[1].toString()), line[1]];
+                if (data.has(i)) data.set(i, [data.get(i)![0].replace(new RegExp(addon[0], "g"), addon[1].toString()), data.get(i)![1]]);
+                else data.set(i, newLine);
+            }
+        }
+
+        let value: ScoreLine[] = [];
+        data.forEach((line) => { value.push(line) });
+
+        return value;
+    }
+
+    /**Hidden scoreboard. */
+    export function setHide(player: ServerPlayer, value: boolean): boolean {
+        if (value) {
+            if (player.hasTag("scoreboard:hide")) return false;
+            else player.addTag("scoreboard:hide");
+            player.sendMessage("§2Scoreboard: §cHide");
+            return true;
+        } else {
+            if (!player.hasTag("scoreboard:hide")) return false;
+            else player.removeTag("scoreboard:hide");
+            player.sendMessage("§2Scoreboard: §aShow");
+            return true
+        }
+    }
+
+    /**Send a scoreboard to player. */
+    export function sendTo(player: ServerPlayer): void {
+        if (player.hasTag("scoreboard:hide")) return;
+        else player.setFakeScoreboard(getRandomTitle(player), getLines(player));
+    }
+
+    /**Save. */
+    export function save(message: boolean = false, actor?: ServerPlayer): void {
+        fs.writeFile(configPath, JSON.stringify(config, null, 2), "utf8", (err) => {
+            if (message) {
+                if (err) {
+                    send.error(`config.json ${err}`, actor);
+                    throw err;
+                }
+                else send.success("config.json Saved!", actor);
+            }
+        });
+    }
+}
